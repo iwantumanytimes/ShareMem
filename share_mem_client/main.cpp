f6157ed2e2d86ed64fe100c3f10b016166fc3fb0
wolverine <860827888@qq.com>
2016-09-12 11:21:01 +0800

    共享内存客户端

---
+++ share_mem_client/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/interprocess/managed_shared_memory.hpp>  
+#include <boost/interprocess/mapped_region.hpp>  
+#include <cstring>  
+#include <iostream>  
+
+using namespace boost::interprocess;
+
+int main(int argc, char *argv[])
+{
+	using namespace boost::interprocess;
+	try {
+		// opening an existing shared memory object   
+		shared_memory_object sharedmem2(open_only, "Hello", read_only);
+
+		// map shared memory object in current address space  
+		mapped_region mmap(sharedmem2, read_only);
+
+		// need to type-cast since get_address returns void*   
+		char *str1 = static_cast<char*> (mmap.get_address());
+		std::cout << str1 << std::endl;
+		shared_memory_object::remove("Hello");
+	}
+	catch (interprocess_exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
+}
